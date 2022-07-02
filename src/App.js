25f46fc7db809703e3150309cc64aad4891c86da
Brannon Starnes <brannlee808@gmail.com>
2022-07-01 22:19:55 -0700

    adds basic placeholders for header, clock, forms, buttons, and dive log

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header";
+import Forms from "./components/forms";
+import Clock from "./components/clock";
+import ClockButtons from "./components/clockButtons";
+import Log from "./components/diveLog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Forms /> 
+      <Clock />
+      <ClockButtons />
+      <Log /> 
+    </>
+  ) ;
 }
 
 export default App;
---
+++ src/components/clock.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Clock(){
+    
+    return(
+        <h1> Clock Goes Here </h1>
+    )
+}
+
+export default Clock;
