422e7abe0d67f7946e58821027b79897a51dca17
Brannon Starnes <brannlee808@gmail.com>
2022-07-06 20:17:08 -0700

    adds charts forms and tableSchedRGD

--- src/components/forms.js
+++ src/components/forms.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 function Forms(){
+
     return (
-    
-            <div class='forms-container'>
+        
+        <div class='forms-container'>
                 <form>
                     <label for='depth'>Depth (fsw): </label>
                     <input class='dive-profile-form' id='depth' type="integer" name='depth'/><br/>
                     <label for='bt'>Bottom Time (min): </label>
                     <input class='dive-profile-form' id='bt' type="integer" name='bt'/><br/>
+                    <input type='submit' value='Get T/S'/>
                 </form>
             </div>             
     );
+    
 }
 export default Forms;
