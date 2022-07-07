30c5fede08b917ac21dad2baf7a759836fb0655b
Brannon Starnes <brannlee808@gmail.com>
2022-07-07 15:16:06 -0700

    form inputs are now passed to log and displayed

--- src/App.js
+++ src/App.js
@@ -6,28 +6,27 @@ import Clock from "./components/clock";
 import ClockButtons from "./components/clockButtons";
 import Log from "./components/diveLog";
 
-
+//TO DO: get depth and bt from the Form, pass it to Log to display
 function App(props) {
-  const [diveLog, setDiveLog] = useState([])
-  const makeDiveLogEntry= (entry) => {
-    let entries = [...diveLog, entry];
-    setDiveLog(entries);
-  }
-
+  
+  const [bottomTime, setBottomTime] = useState([]) 
+  const [depth, setDepth] = useState([])
+  const [onO2, setOnO2] = useState([])
+  const [leftSurface, setLS] = useState([])
 
   const getBottomTime = (leftSurf, leftBott) => {
     let bottomTime = leftSurf - leftBott
-    return bottomTime
+    return setBottomTime(bottomTime)
   }
 
 
   return (
     <>
       <Header />
-      <Forms /> 
+      <Forms getBT={getBottomTime} setDepth={setDepth} setBT={setBottomTime} depth={depth} BT={bottomTime}/> 
       <Clock />
-      <ClockButtons getBT={getBottomTime}/>
-      <Log makeDiveLogEntry={makeDiveLogEntry}/> 
+      <ClockButtons getBT={getBottomTime} setDepth={setDepth} setOnO2={setOnO2} setLS={setLS} />
+      <Log LS={leftSurface} BT={bottomTime} D={depth} onO2={onO2} /> 
     </>
   ) ;
 }
--- src/components/diveLog.js
+++ src/components/diveLog.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 function Log(props){
+    
+    const [diveLog, setDiveLog] = useState([])
+    
+    const makeDiveLogEntry= (entry) => {
+    let entries = [...diveLog, entry];
+    setDiveLog(entries);
+  }
+    
+    // const log = makeDiveLogEntry()
+    // const mapEntries = (log.map(entry => {
+    //     return <li>{entry}</li>
+    // }))
+
+
     return(
         <div id='dive-log-container'>
             <title> Diving Log </title>
             <div>
                 <h3> Dive Log Stuff Goes Here </h3>
+                <h1>{props.D || "0"} fsw</h1>
+                <h1>{props.BT || "0"} BT</h1>
+                {/* <ul>
+                    {mapEntries}
+                </ul> */}
             </div>
         </div>
     )
